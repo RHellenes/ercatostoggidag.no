521f2c308436815af39095db2075e559b3d6b32f
René Hellenes <rene.hellenes@gmail.com>
2022-10-08 10:34:18 +0200

    Improved results by creating a group of ids to giphs

--- app.vue
+++ app.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="container">
-    <dl :class="`style-${getHeadingStyle}`" >
+    <dl :class="`style-${getStyleId}`" >
       <dt class="wordart">
         <div class="preview" data-content="Er Cato støgg i dag?">
           Er Cato støgg i dag?
         </div>
       </dt>
-      <dd>
-        <template v-if="giphy.data">
+      <dd v-if="giphy.data">
           <Transition name="popin">
-            <img v-show="shouldShow" :srcset="giphy.data.images.original.webp" width="500" height="500" alt="Cato er alltid støgg"> 
+            <img v-show="shouldShow" :srcset="giphy.data.images.original.webp" width="500" height="500" alt="Cato er alltid støgg" /> 
           </Transition>
-        </template>
-        <template v-else>
+        </dd>
+        <dd v-else>
           <p>Ops! Han er så støgg at vi ikke kunne finne en giph som sa ja tydlig nok!</p>
-        </template>
       </dd>
       <button v-show="!shouldShow" @click="handleShouldShow" style="z-index:1000000">Finn svaret</button>
     </dl>
@@ -23,14 +21,44 @@
 <script setup lang="ts">
 import confetti from 'canvas-confetti'
 
+const ids: string[] = [
+  '70YaDoZ1VqBZ8SgYiz',
+  '70YaDoZ1VqBZ8SgYiz',
+  '3ohhweiVB36rAlqVCE',
+  '2RGhmKXcl0ViM',
+  'J336VCs1JC42zGRhjH',
+  'XMBJ0l20sNWEM',
+  'ynRrAHj5SWAu8RA002',
+  'dvk2pccSeR4Gz9ABl8',
+  'nFjDu1LjEADh6',
+  'jErnybNlfE1lm',
+  'Zai3ffKrUcLFwalDor',
+  'h7CrjtmsLtqxi',
+  '57ZSazimN4opZYkhjc',
+  'XMBJ0l20sNWEM',
+  'ynRrAHj5SWAu8RA002',
+  'SH8YML3ofBvz7qPB1R',
+  'hWFVffRsJUm2zKaEbE',
+  'dvk2pccSeR4Gz9ABl8',
+  'LNxlSJpxAN55bS51Ro',
+  'XMBJ0l20sNWEM',
+  '6cFcUiCG5eONW',
+  '6cFcUiCG5eONW',
+  'l8sXa4DqpPvs6aZ0t2'
+]
+
+const getRandomNumber = ((topLimit:number) => {
+  return Math.floor(Math.random() * topLimit) + 1
+})
 
 const shouldShow = ref(false)
-const {data: giphy} = await useFetch('https://api.giphy.com/v1/gifs/random?api_key=9quZVPwsdg1SFp7953XVDFZu0HfGvQSb&tag=big+yes&rating=g')
+const {data: giphy} = await useFetch(`https://api.giphy.com/v1/gifs/${ids[getRandomNumber(ids.length)]}?api_key=9quZVPwsdg1SFp7953XVDFZu0HfGvQSb`)
 
 
-const getHeadingStyle = computed(() => {
-  return Math.floor(Math.random() * 21) + 1
+const getStyleId = computed(() => {
+  return getRandomNumber(21)
 })
+
 
 const handleShouldShow = (): void => {
   shouldShow.value = true
